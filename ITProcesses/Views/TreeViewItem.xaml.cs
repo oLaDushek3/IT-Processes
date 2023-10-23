7f13c3bb5d81d5af6aac876d08a89dd6a08c5bde
oLaDushek3 <ilyakuzin2004@yandex.ru>
2023-10-23 14:21:56 +0300

    Add new files

---
+++ ITProcesses/Views/TreeViewItem.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace ITProcesses.ViewModels;
+
+public partial class TreeViewItem : UserControl
+{
+    public TreeViewItem()
+    {
+        InitializeComponent();
+    }
+}
