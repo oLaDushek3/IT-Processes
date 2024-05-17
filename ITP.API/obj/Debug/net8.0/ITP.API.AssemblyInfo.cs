f7c3257f534e2a80a119d0172ae506bf8828d16a
arshi <arshinov04@gmail.com>
2024-05-17 17:34:43 +0300

    сячясяч

--- ITP.API/obj/Debug/net8.0/ITP.API.AssemblyInfo.cs
+++ ITP.API/obj/Debug/net8.0/ITP.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITP.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e8193be01a9050a16f1ea44efdbfebd8e432b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITP.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITP.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
